36dbf6288a222c6dc1dc946c41ff2a9c32f8d5e4
AlamgirShagor <freelancershagor@gmail.com>
2021-01-07 05:10:03 -0800

    done

--- src/App.js
+++ src/App.js
@@ -8,43 +8,39 @@ import {
 } from "react-router-dom";
 import { useState } from 'react';
 import StepOne from './Components/StepOne/StepOne';
-import Login from './Components/Login/Login'
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import NotFound from './Components/NotFound/NotFound'
 import StepAll from './Components/StepAll/StepAll';
 import Sidebar from './Components/Sidebar/Sidebar';
-import Home from './Components/Home/Home';
 
 export const UserContext = createContext();
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState({});
   const [loggedInInput, setLoggedInInput] = useState({});
   const [currentStep, setCurrentStep] = useState(0);
   const [activeStep, setActiveStep] = React.useState(3);
   
   return (
-    <UserContext.Provider value={[loggedInUser, setLoggedInUser, loggedInInput, setLoggedInInput, currentStep, setCurrentStep]}>
+    <UserContext.Provider value={[loggedInInput, setLoggedInInput, currentStep, setCurrentStep]}>
       <Router>
       <Switch>
         <Route exact path="/">
-          <Home></Home>
+          <Dashboard></Dashboard>
         </Route>
-        <PrivateRoute exact path="/BasicInformation">
+        <Route exact path="/BasicInformation">
           <Dashboard></Dashboard>
-        </PrivateRoute>
-        <PrivateRoute exact path="/AdditionalDetails">
+        </Route>
+        <Route exact path="/AdditionalDetails">
           <Dashboard></Dashboard>
-        </PrivateRoute>
-        <PrivateRoute exact path="/LicenseeInformation">
+        </Route>
+        <Route exact path="/LicenseeInformation">
           <Dashboard></Dashboard>
-        </PrivateRoute>
-        <PrivateRoute exact path="/DSS/OMBUDSMAN">
+        </Route>
+        <Route exact path="/DSS/OMBUDSMAN">
           <Dashboard></Dashboard>
-        </PrivateRoute>
-        <PrivateRoute exact path="/PersonInCharge">
+        </Route>
+        <Route exact path="/PersonInCharge">
           <Dashboard></Dashboard>
-        </PrivateRoute>
+        </Route>
         <Route exact path="/StepOne">
           <StepOne></StepOne>
         </Route>
@@ -54,11 +50,8 @@ function App() {
         <Route exact path="/sidebar">
           <Sidebar></Sidebar>
         </Route>
-        <PrivateRoute path="/Dashboard">
+        <Route path="/Dashboard">
           <Dashboard></Dashboard>
-        </PrivateRoute>
-        <Route exact path="/Login" >
-          <Login></Login>
         </Route>
         <Route path="*">
             <NotFound></NotFound>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,53 +1,19 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { UserContext } from '../../App';
 import logo from '../../image/L4.jpg'
-import { useHistory, useLocation } from 'react-router-dom';
-
-import { initializeLoginFramework, handleSignOut} from '../Login/loginManager';
 const Header = () => {
-    initializeLoginFramework();
-    const [newUser, setNewUser] = useState(false);
-    const [user, setUser] = useState({
-        isSignedIn: false,
-        name: '',
-        email: '',
-        password: '',
-        photo: ''
-      });
-      
-    const [loggedInUser, setLoggedInUser ] = useContext(UserContext);
-    const history = useHistory();
-    const location = useLocation();
-    let { from } = location.state || { from: { pathname: "/" } };
-
-    const signOut = () => {
-        handleSignOut()
-        .then(res => {
-            handleResponse(res, false);
-        })
-    }
     
-
-    const handleResponse = (res, redirect) =>{
-        setUser(res);
-        setNewUser(true);
-        setLoggedInUser(res);
-        if(redirect){
-            history.replace(from);
-        }
-      }
     return (
         <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#1B74D0"}}>
             <Link to="/">
                 <img style={{width: "100px"}} src={logo} alt=""/>
             </Link>
-        <div className="ml-auto">
-        { newUser ?  <button className="btn" style={{backgroundColor: "#4ABC7B"}}>
-                <Link to="/Dashboard" className="text-white">
-                        <span>Sign in</span>
-                </Link>
-            </button> : <button className="btn" style={{backgroundColor: "#4ABC7B", color: "#fff"}} onClick={signOut}>Sign Out</button>} 
+        <div className="ml-auto"> 
+            <button className="btn" style={{backgroundColor: "#4ABC7B"}}>
+                    <Link to="/Dashboard" className="text-white">
+                            <span>Sign in</span>
+                    </Link>
+            </button>
         </div>
         </nav>
     );
